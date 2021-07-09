84b06ab54a0331ad35400b1e3e28d0898918e2c8
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-09 12:20:29 +0300

    add the logout url route

--- doubletap/views.py
+++ doubletap/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-@login_required(login_url='/accounts/login/')
+@login_required(login_url='/accounts/register/')
 def index(request):
 
     return render(request,'index.html')
