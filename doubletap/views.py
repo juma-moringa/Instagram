cece38b319be550536cfc8dfb79a4ac5bf652756
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-10 00:52:02 +0300

    define the profile function in the views.py

--- doubletap/views.py
+++ doubletap/views.py
@@ -1,16 +1,16 @@
 from django.contrib.auth import authenticate, login
 from django.http import request
-from doubletap.forms import SignUpForm
+from doubletap.forms import SignUpForm, UserProfileUpdateForm, UserprofileForm
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+    #mainpage
 @login_required(login_url='/accounts/login/')
 def index(request):
     return render(request,'index.html')
 
-
+    # registration function
 def register(request):
     if request.method=="POST":
         form=SignUpForm(request.POST)
@@ -25,3 +25,17 @@ def register(request):
     else:
         form= SignUpForm()
     return render(request, 'registration/registration_form.html', {"form":form})        
+
+    #user profile function    
+@login_required(login_url='/accounts/login/')    
+def insta_profile(request):
+    if request.method == 'POST':
+        profile_form = UserprofileForm(request.POST, request.FILES, instance=request.user)
+        if  profile_form.is_valid():
+            profile_form.save()
+            return redirect('home')
+    else:
+        profile_form = UserprofileForm(instance=request.user)
+        user_form = UserProfileUpdateForm(instance=request.user)
+    return render(request, 'profile.html',{ "profile_form": profile_form,"user_form":user_form})
+         
