774b92b82417e6bc12687c4b5342f7ec4266e69c
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-10 00:43:05 +0300

    add the userprofile and update user profile forms.

--- doubletap/forms.py
+++ doubletap/forms.py
@@ -1,13 +1,25 @@
 
+from doubletap.models import Profile
 from django.contrib.auth.models import User
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-
+#signform
 class SignUpForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ['username', 'email', 'password1', 'password2']
 
-   
+#user profile form
+class UserprofileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [ 'profile_photo', 'bio']  
+
+#update profile form
+class UserProfileUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username','email']   
