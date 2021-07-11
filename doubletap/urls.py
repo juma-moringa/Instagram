91f316bd761c91e797ec18cdf449c43abd03b944
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-11 22:17:48 +0300

    styled the navbar and added links

--- doubletap/urls.py
+++ doubletap/urls.py
@@ -7,8 +7,10 @@ urlpatterns=[
     url(r'^$',views.index, name='index'),
     url('register/',views.register, name='registration'),
     url('login/',auth_views.LoginView.as_view(), name='login'),
+    url('logout/',auth_views.LogoutView.as_view(),name='logout'),
     url('profile/',views.insta_profile, name='profile'),
     url('Update_insta_Profile/',views.Update_insta_Profile, name='update_profile'),
     url('gramblog/', views.Post_gram, name='Postblog'),
     url('search/', views.search_profile,name='search'),
+    
 ]
