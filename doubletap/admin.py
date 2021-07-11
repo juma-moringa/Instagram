efd9fb627c514adebd49383391ccd4e5b7c469dc
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-11 12:11:01 +0300

    register the models in the admin.py and craete super user

--- doubletap/admin.py
+++ doubletap/admin.py
@@ -1,3 +1,9 @@
+from doubletap.models import Comment, Follow, Image, Profile
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Follow)
+
