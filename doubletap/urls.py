8d742a7ecea75962073f81cb5a5be62c7f58c50a
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-12 18:09:58 +0300

    add the urlpatterns settings in the application urls.py

--- doubletap/urls.py
+++ doubletap/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.conf import settings
 
 urlpatterns=[
     url(r'^$',views.index, name='index'),
@@ -14,3 +15,5 @@ urlpatterns=[
     url('search/', views.search_profile,name='search'),
     
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
