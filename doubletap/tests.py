ecaaa11d865d31524e7db9334a1cb5e265d5dbf7
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-12 14:39:33 +0300

    run profile unit tests

--- doubletap/tests.py
+++ doubletap/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 
 from django.contrib.auth.models import User
 # Create your tests here.
-
+#test1
 class ImageTestClass(TestCase):
     """
 
@@ -29,4 +29,26 @@ class ImageTestClass(TestCase):
         Profile.objects.all().delete()
         User.objects.all().delete()
         Image.objects.all().delete()
-        
+#test2
+class ProfileTestClass(TestCase):
+    '''
+    test class for Profile model unit tests.
+    '''
+    def setUp(self):
+        self.user = User.objects.create_user("username", "password")
+        self.profile_test = Profile(profile_photo='sampletestpic.png',bio="kizuri ndio kina garamiwa",user=self.user)
+        self.profile_test.save()
+
+    def test_instance_true(self):
+        self.profile_test.save()
+        self.assertTrue(isinstance(self.profile_test, Profile)) 
+
+    def test_save_profile(self):
+        self.profile_test.save_profile()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) > 0)
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Profile.objects.all().delete()
+        User.objects.all().delete()       
